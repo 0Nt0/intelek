e0b2acd02683e9d4440d6f3612aa2cfdfd27bb33
ToriNtHr <toma.ziliute@stud.viko.lt>
2023-01-24 15:30:02 +0200

    +

---
+++ 1Darbas/1Darbas/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿
+namespace _1Darbas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(32, 164);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(346, 231);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(608, 401);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(145, 34);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Vykdyti";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "e7 ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 410);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "     ";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(41, 115);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "e";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(113, 115);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 17);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "x1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(197, 115);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(22, 17);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "x2";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(273, 115);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(43, 17);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Klasė";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 17);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "e8 ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(134, 27);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(33, 22);
+            this.textBox3.TabIndex = 21;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(99, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 17);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "x1";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(134, 64);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(33, 22);
+            this.textBox4.TabIndex = 23;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(99, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 17);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "x1";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(225, 28);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(33, 22);
+            this.textBox5.TabIndex = 25;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(190, 29);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(22, 17);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "x2";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(225, 65);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(33, 22);
+            this.textBox6.TabIndex = 27;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(190, 66);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(22, 17);
+            this.label11.TabIndex = 26;
+            this.label11.Text = "x2";
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AutoScroll = true;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(407, 164);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(346, 231);
+            this.flowLayoutPanel2.TabIndex = 28;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(350, 410);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(28, 17);
+            this.label14.TabIndex = 33;
+            this.label14.Text = "     ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(316, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(33, 22);
+            this.textBox1.TabIndex = 35;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(281, 28);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(19, 17);
+            this.label12.TabIndex = 34;
+            this.label12.Text = "k:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ 1Darbas/1Darbas/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using System.Collections;
+
+namespace _1Darbas
+{
+    public partial class Form1 : Form
+    {
+        
+        SqlConnection con = new SqlConnection("Data Source=localhost;Initial Catalog=Intelektikos1;Integrated Security=True");
+        ArrayList array = new ArrayList();
+        ArrayList array2 = new ArrayList();
+        ArrayList array3 = new ArrayList();
+        ArrayList array4 = new ArrayList();
+        ArrayList simbArray = new ArrayList();
+        ArrayList simbArray3 = new ArrayList();
+        ArrayList simbArray2 = new ArrayList();
+        ArrayList simbArray4 = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            flowLayoutPanel2.Controls.Clear();
+            int pl = 0, mn = 0, nan = 0;
+            int pl2 = 0, mn2 = 0, nan2 = 0;
+
+            string e7x1=textBox3.Text, e8x1=textBox4.Text, e7x2=textBox5.Text, e8x2=textBox6.Text;
+            int k = Convert.ToInt32(textBox1.Text);
+            if(String.IsNullOrEmpty(e7x1)||String.IsNullOrWhiteSpace(e7x1)||String.IsNullOrWhiteSpace(textBox1.Text)||String.IsNullOrEmpty(textBox1.Text) ||String.IsNullOrEmpty(e8x1) || String.IsNullOrWhiteSpace(e8x1)|| String.IsNullOrEmpty(e7x2) || String.IsNullOrWhiteSpace(e7x2)|| String.IsNullOrEmpty(e8x2) || String.IsNullOrWhiteSpace(e8x2))
+            {
+                MessageBox.Show("Negali būti tuščia");
+            }
+
+            con.Open();
+            SqlCommand command2 = new SqlCommand("Select *  from mokymo_imtis ", con);
+            SqlDataReader DataRead = command2.ExecuteReader();
+            while(DataRead.Read())
+            {
+                array.Add(Math.Sqrt(Math.Pow(Convert.ToInt32(e7x1) - Convert.ToInt32(DataRead["x1"].ToString()),2)+Math.Pow(Convert.ToInt32(e7x2)-Convert.ToInt32(DataRead["x2"].ToString()),2)));
+                Label ItemPic3 = new Label();
+                ItemPic3.Width = 250;
+                ItemPic3.Height = 30;
+                ItemPic3.BackColor = Color.FromArgb(100, 100, 100, 100);
+                ItemPic3.ForeColor = Color.FromArgb(0, 0, 0, 0);
+                ItemPic3.Text = "D(e7,"+DataRead["e"].ToString()+ ")=­­­Sqrt(("+e7x1+"-"+DataRead["x1"].ToString()+")^2+("+e7x2+"-"+DataRead["x2"].ToString()+")^2)="+(Math.Sqrt(Math.Pow(Convert.ToInt32(e7x1) - Convert.ToInt32(DataRead["x1"].ToString()), 2) + Math.Pow(Convert.ToInt32(e7x2) - Convert.ToInt32(DataRead["x2"].ToString()), 2))).ToString();
+                ItemPic3.TextAlign = ContentAlignment.TopLeft;
+                flowLayoutPanel2.Controls.Add(ItemPic3);
+            }
+            DataRead.Close();
+
+            Label ItemPic4 = new Label();
+            ItemPic4.Width = 250;
+            ItemPic4.Height = 30;
+            ItemPic4.BackColor = Color.FromArgb(100, 100, 100, 100);
+            ItemPic4.ForeColor = Color.FromArgb(0, 0, 0, 0);
+            ItemPic4.Text = "       ";
+            ItemPic4.TextAlign = ContentAlignment.TopLeft;
+            flowLayoutPanel2.Controls.Add(ItemPic4);
+
+            SqlDataReader DataRead2 = command2.ExecuteReader();
+            while (DataRead2.Read())
+            {
+                array2.Add(Math.Sqrt(Math.Pow(Convert.ToInt32(e8x1) - Convert.ToInt32(DataRead2["x1"].ToString()), 2) + Math.Pow(Convert.ToInt32(e8x2) - Convert.ToInt32(DataRead2["x2"].ToString()), 2)));
+                Label ItemPic5 = new Label();
+                ItemPic5.Width = 250;
+                ItemPic5.Height = 30;
+                ItemPic5.BackColor = Color.FromArgb(100, 100, 100, 100);
+                ItemPic5.ForeColor = Color.FromArgb(0, 0, 0, 0);
+                ItemPic5.Text = "D(e8," + DataRead2["e"].ToString() + ")=­­­Sqrt((" + e8x1 + "-" + DataRead2["x1"].ToString() + ")^2+(" + e8x2 + "-" + DataRead2["x2"].ToString() + ")^2)=" + (Math.Sqrt(Math.Pow(Convert.ToInt32(e8x1) - Convert.ToInt32(DataRead2["x1"].ToString()), 2) + Math.Pow(Convert.ToInt32(e8x2) - Convert.ToInt32(DataRead2["x2"].ToString()), 2))).ToString();
+                ItemPic5.TextAlign = ContentAlignment.TopLeft;
+                flowLayoutPanel2.Controls.Add(ItemPic5);
+            }
+            array2.Add(Math.Sqrt(Math.Pow(Convert.ToInt32(e8x1) - Convert.ToInt32(e7x1), 2) + Math.Pow(Convert.ToInt32(e8x2) - Convert.ToInt32(e7x2), 2)));
+
+            Label ItemPic6 = new Label();
+            ItemPic6.Width = 250;
+            ItemPic6.Height = 30;
+            ItemPic6.BackColor = Color.FromArgb(100, 100, 100, 100);
+            ItemPic6.ForeColor = Color.FromArgb(0, 0, 0, 0);
+            ItemPic6.Text = "D(e8, e7)=­­­Sqrt((" + e8x1 + "-" + e7x1 + ")^2+(" + e8x2 + "-" + e7x2 + ")^2)=" + (Math.Sqrt(Math.Pow(Convert.ToInt32(e8x1) - Convert.ToInt32(e7x1), 2) + Math.Pow(Convert.ToInt32(e8x2) - Convert.ToInt32(e7x2), 2))).ToString();
+            ItemPic6.TextAlign = ContentAlignment.TopLeft;
+            flowLayoutPanel2.Controls.Add(ItemPic6);
+
+            DataRead2.Close();
+            array.Sort();
+            array2.Sort();
+            
+            int counter = 0;
+            foreach (double i in array)
+            {
+                if (counter < k)
+                {
+                    array3.Add(i);
+                    counter++;
+                }
+                else break;
+            }
+
+            int counter2 = 0;
+            foreach (double i in array2)
+            {
+                
+                if (counter2 < k)
+                {
+                    array4.Add(i);
+                    counter2++;
+                }
+                else break;
+            }
+            /////////////////////////////////////////////
+            SqlCommand command3 = new SqlCommand("Select *  from mokymo_imtis ", con);
+           SqlDataReader DataRead3 = command3.ExecuteReader();
+            while (DataRead3.Read())
+            {
+               foreach(double i in array3)
+                {
+                    
+                    if ((Math.Sqrt(Math.Pow(Convert.ToInt32(e7x1) - Convert.ToInt32(DataRead3["x1"].ToString()), 2) + Math.Pow(Convert.ToInt32(e7x2) - Convert.ToInt32(DataRead3["x2"].ToString()), 2)))==i)
+                    {
+                        simbArray.Add(DataRead3["class"].ToString());
+                        
+                    }
+                }
+            }
+            DataRead3.Close();
+
+            simbArray.Sort();
+            ///////////////////////////////////////////////////
+            int counter3 = 0;
+
+            foreach (string i in simbArray)
+            {
+                if (counter3 < k)
+                {
+                    simbArray3.Add(i);
+                    counter3++;
+                }
+                else break;
+            }
+
+            foreach (string i in simbArray3)
+            {
+                if (i == "+")
+                {
+                    pl++;
+                }
+                else if (i == "-")
+                { mn++; }
+                else if(i=="nan") 
+                { nan++; }
+            }
+            if (pl > mn)
+            {
+                label5.Text = "e7 klasė: +";
+                SqlCommand command11 = new SqlCommand("Insert mokymo_imtis( e, x1,x2, class)VALUES('e7','" + e7x1 + "','" + e7x2 + "', '+')", con);
+                command11.ExecuteNonQuery();
+            }
+            else if (pl < mn)
+            {
+                label5.Text = "e7 klasė: -";
+                SqlCommand command11 = new SqlCommand("Insert mokymo_imtis( e, x1,x2, class)VALUES('e7','" + e7x1 + "','" + e7x2 + "', '-')", con);
+                command11.ExecuteNonQuery();
+            }
+            else if(pl==mn)
+            {
+                label5.Text = "e7 klasė: nežinoma";
+                SqlCommand command11 = new SqlCommand("Insert mokymo_imtis( e, x1,x2, class)VALUES('e7','" + e7x1 + "','" + e7x2 + "', 'nan')", con);
+                command11.ExecuteNonQuery();
+            }
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            int counter1_5 = 0;
+            string eLt="";
+              SqlCommand command4 = new SqlCommand("Select *  from mokymo_imtis ", con);
+              SqlDataReader DataRead4 = command4.ExecuteReader();
+
+              while (DataRead4.Read())
+              {
+                  foreach (double i in array4)
+                  {
+                 
+                    if ((Math.Sqrt(Math.Pow(Convert.ToInt32(e8x1) - Convert.ToInt32(DataRead4["x1"].ToString()), 2) + Math.Pow(Convert.ToInt32(e8x2) - Convert.ToInt32(DataRead4["x2"].ToString()), 2))) == i && counter1_5<k && eLt!=DataRead4["e"].ToString())
+                     {
+                        simbArray2.Add(DataRead4["class"].ToString());
+                        counter1_5++;
+                        eLt = DataRead4["e"].ToString();
+                     }
+                  }
+              }
+              DataRead4.Close();
+
+            simbArray.Sort();
+
+            int counter4 = 0;
+           
+            foreach (string i in simbArray2)
+            {
+                if (counter4 < k)
+                {
+                    simbArray4.Add(i);
+                    counter4++;
+                }
+                else break;
+            }
+
+            foreach (string i in simbArray4)
+            {
+               
+                if (i == "+")
+                {
+                    pl2++;
+                }
+                else if (i == "-")
+                { mn2++; }
+                else if(i=="nan") 
+                { nan2++; }
+            }
+          
+            if (pl2 > mn2 && pl2>nan2 )
+            {
+                
+               label14.Text = "e8 klasė: +";
+               
+               
+            }
+            else if (pl2 < mn2 && mn2> nan2 )
+            {
+
+               
+                 label14.Text = "e8 klasė: -" ;
+
+            }
+            else 
+            {
+                
+                 label14.Text = "e8 klasė: nežinoma" ;
+
+            }
+           
+            
+
+            SqlCommand command0 = new SqlCommand("Delete from mokymo_imtis where e='e7'", con);
+            command0.ExecuteNonQuery();
+            con.Close();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand command = new SqlCommand("Select *  from mokymo_imtis ", con);
+            SqlDataReader DataRead = command.ExecuteReader();
+            while (DataRead.Read())
+            {
+                Label ItemPic2 = new Label();
+                ItemPic2.Width = 250;
+                ItemPic2.Height = 20;
+                ItemPic2.BackColor = Color.FromArgb(100, 100, 100, 100);
+                ItemPic2.ForeColor = Color.FromArgb(0, 0, 0, 0);
+                ItemPic2.Text = DataRead["e"].ToString() + "                " + DataRead["x1"].ToString() + "                    " + DataRead["x2"].ToString() + "                   " + DataRead["class"].ToString();
+                ItemPic2.TextAlign = ContentAlignment.TopLeft;
+
+                flowLayoutPanel1.Controls.Add(ItemPic2);
+            }
+
+          
+
+            DataRead.Close();
+            con.Close();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
